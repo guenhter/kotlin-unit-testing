04a1abb5bd69bedd98cfda2b11f1f619a45830cf
guenhter <grill.guenther@gmail.com>
2018-05-01 14:01:13 +0200

    Restructure dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,20 +18,28 @@ var junit5UnrollVersion = "0.1.2"
 dependencies {
     compile(kotlin("stdlib-jdk8"))
 
+
+    // junit5 dependencies
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testRuntime("org.junit.platform:junit-platform-launcher:$junitPlatformLauncherVersion")
+
+
+    // spek dependencies
     testCompile("org.jetbrains.spek:spek-api:$spekVersion")
     testCompile("org.jetbrains.spek:spek-subject-extension:$spekVersion")
     testCompile("org.jetbrains.spek:spek-data-driven-extension:$spekVersion")
 
+    testRuntime("org.jetbrains.spek:spek-junit-platform-engine:$spekVersion")
+
+
+    // other nice junit and testing dependencies
+    testCompile("com.github.blindpirate:junit5-unroll-extension:$junit5UnrollVersion")
     testCompile("org.assertj:assertj-core:$assertJVersion")
     testCompile("org.amshove.kluent:kluent:$kluentVersion")
 
-    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-    testCompile("com.github.blindpirate:junit5-unroll-extension:$junit5UnrollVersion")
-
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-    testRuntime("org.junit.platform:junit-platform-launcher:$junitPlatformLauncherVersion")
-    testRuntime("org.jetbrains.spek:spek-junit-platform-engine:$spekVersion")
 }
 
 tasks.withType<KotlinCompile> {
